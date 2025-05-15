5b6e6153219ce10da27c6adaab89ac16cb600309
adriánleyes <leyesadrian7@gmail.com>
2025-05-15 20:24:49 -0300

    Tercer commit - Navega las Rutas

--- Item.jsx
+++ Item.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
+import '../styles/Item.css';
 
 function Item({ item }) {
   return (
-    <div className="item">
+    <div className="item-card">
       <h3>{item.nombre}</h3>
       <p>{item.descripcion}</p>
-      <Link to={/item/${item.id}}>Ver detalle</Link>
+      <p><strong>${item.precio}</strong></p>
+      <Link to={/item/${item.id}} className="detail-button">Ver Detalle</Link>
     </div>
   );
 }
+
+export default Item;
--- NavBar.jsx
+++ NavBar.jsx
@@ -1,12 +1,18 @@
 import { NavLink } from 'react-router-dom';
+import '../styles/NavBar.css';
 
 function NavBar() {
   return (
-    <nav>
-      <NavLink to="/">Todos</NavLink>
-      <NavLink to="/categoria/tableros">Tableros</NavLink>
-      <NavLink to="/categoria/piezas">Piezas</NavLink>
-      <NavLink to="/categoria/relojes">Relojes</NavLink>
+    <nav className="navbar">
+      <h1 className="logo">Tienda Ajedrez</h1>
+      <div className="nav-links">
+        <NavLink to="/" className="nav-link">Inicio</NavLink>
+        <NavLink to="/categoria/tableros" className="nav-link">Tableros</NavLink>
+        <NavLink to="/categoria/piezas" className="nav-link">Piezas</NavLink>
+        <NavLink to="/categoria/relojes" className="nav-link">Relojes</NavLink>
+      </div>
     </nav>
   );
 }
+
+export default NavBar;
